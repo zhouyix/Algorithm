12f22322c4138f3520e5651b51e68cfbe24d6ad1
Zhou Yi <zhouyi_naive@163.com>
2017-01-18 21:09:04 +0800

    LongestSubstringWithoutRepeatingCharacters

---
+++ LeetcodeLongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] map = new int[256];  
+        int j = 0;
+        int i = 0;
+        int ans = 0;
+        for (i = 0; i < s.length(); i++) {
+            while (j < s.length() && map[s.charAt(j)]==0) {
+                map[s.charAt(j)] = 1;
+                ans = Math.max(ans, j-i + 1);
+                j ++;
+            }
+            map[s.charAt(i)] = 0;
+        }
+        
+        return ans;
+    }
+}
