8cae5f3d7bb5277516bc8db74575f80a4c050163
Zhou Yi <zhouyi_naive@163.com>
2017-03-06 20:07:25 +0800

    Implement strStr

---
+++ LeetcodeImplementstrStr.java
@@ -0,0 +1,19 @@
+ public int strStr(String source, String target) {
+        if (source == null || target == null) {
+            return -1;
+        }
+        
+        for (int i = 0; i < source.length() - target.length() + 1; i++) {
+            int j = 0;
+            for (j = 0; j < target.length(); j++) {
+                if (source.charAt(i + j) != target.charAt(j)) {
+                    break;
+                }
+            }
+            // finished loop, target found
+            if (j == target.length()) {
+                return i;
+            }
+        }
+        return -1;
+    }
