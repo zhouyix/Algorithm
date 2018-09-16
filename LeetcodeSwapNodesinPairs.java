f312e4a98942284b6cb16fa677754b879b1e558a
zhouy <zhouy@microsoft.com>
2018-09-15 22:33:52 -0700

    18.09.15

--- LeetcodeSwapNodesinPairs.java
+++ LeetcodeSwapNodesinPairs.java
@@ -22,4 +22,14 @@ public class Solution {
 	        
 	        return h;
     }
+	
+	
+	 public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode next = head.next;
+        head.next = swapPairs(head.next.next);
+        next.next = head;
+        return next;
+    }
 }
